ef97929c4b7e4a13221d42c79626f7b38710a52d
op-rahul <rahul.dhole@onpharma.org>
2025-05-06 14:23:29 +0000

    Initial commit

---
+++ msw/server.ts
@@ -0,0 +1,32 @@
+// ~/msw/server.ts
+import { http, HttpResponse } from "msw";
+
+export default defineNuxtMswServerOption(() => {
+  // assume your server listening at http://localhost:3000
+  const baseURL = "https://3jdr8d-3000.csb.app";
+
+  // composables that not related to NuxtApp can be used here, like: useRuntimeConfig
+
+  const handlers = [
+    // Intercept "GET http://localhost:3000/user" requests...
+    http.get(baseURL + "/api/user2", () => {
+      // ...and respond to them using this JSON response.
+      return HttpResponse.json({
+        message: "Hello msw server!",
+      });
+    }),
+  ];
+  return {
+    baseURL, // baseURL is required
+    handlers,
+    serverOptions: {
+      onUnhandledRequest: "bypass",
+    },
+
+    onRequest(mswServer, h3Event) {
+      // This funtion will be call when Nitro server "request" hook
+      console.log("Hello from onRequest");
+      mswServer.use(/*...*/);
+    },
+  };
+});
---
+++ msw/worker.ts
@@ -0,0 +1,33 @@
+// ~/msw/worker.ts
+import { http, HttpResponse } from "msw";
+
+export default defineNuxtMswWorkerOption(() => {
+  const handlers = [
+    // Intercept "GET /api/user2" requests...
+    http.get("/api/user2", () => {
+      // ...and respond to them using this JSON response.
+      return HttpResponse.json({
+        message: "Hello Worker!",
+      });
+    }),
+  ];
+  // You can access any browser api
+  // window.location.href
+
+  return {
+    handlers,
+    workerOptions: {
+      // ...you can pass options to worker.start()
+      // onUnhandledRequest: 'bypass',
+    },
+    onWorkerStarted(worker, nuxtApp) {
+      // Module will setup worker when nuxt run client plugin
+      // Means this function will be called after plugin call worker.start()
+
+      nuxtApp.hook("app:mounted", () => {
+        // const route = useRoute()
+        // console.log(worker.listHandlers())
+      });
+    },
+  };
+});
